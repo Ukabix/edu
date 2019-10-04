f4abf95bf5fb75edb57a9de32000383da90e0e84
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 13:43:48 +0200

    04102019

---
+++ operators/comparisons.py
@@ -0,0 +1,25 @@
+print("equality")
+
+print(2 == 2)
+
+print(2 == 1)
+
+print('hello' == 'bye')
+
+print('Bye' == 'bye') # case sens
+
+print('2' == 2) # type sens
+
+print(2.0 == 2)
+
+print("nonequality")
+
+print(3 != 3)
+
+print(4 != 5)
+
+print(1 > 3)
+
+print(3 >= 3)
+
+print(3 >= 4)
---
+++ operators/logical - chain operators.py
@@ -0,0 +1,20 @@
+# and or not
+
+print(1 < 2)
+print(2 < 3)
+print(1 < 2 < 3)
+print(1 < 2 > 3) # domyślnie and
+
+print("and")
+
+print(1 < 2 and 2 > 3)
+print(1 < 2 and 2 < 3)
+print('h' == 'h' and 2 == 2)
+
+print("or")
+print(1 < 2 or 2 > 3)
+print(1 > 2 or 2 > 3)
+
+print("not")
+print(not 1 > 2)
+print(not 2 < 3)
