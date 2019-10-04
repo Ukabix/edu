2aefb0a157c017042429a5c5322e551be7165a72
Łukasz Piliszek <54454097+Ukabix@users.noreply.github.com>
2019-10-04 09:39:01 +0200

    Rename formatowanie tekstu.py to objects/text formatting.py

--- formatowanie tekstu.py
+++
@@ -1,1 +0,0 @@
-print("Plynie Wisła, płynie \n\tPo polskiej krainie, \n\t\tZobaczyła Kraków, \n\t\tpewno go nie minie \n\t\tPewno go nie minie. \nJak go zobaczyła \n\tTak go pokochała")
---
+++ objects/text formatting.py
@@ -0,0 +1,1 @@
+print("Plynie Wisła, płynie \n\tPo polskiej krainie, \n\t\tZobaczyła Kraków, \n\t\tpewno go nie minie \n\t\tPewno go nie minie. \nJak go zobaczyła \n\tTak go pokochała")
