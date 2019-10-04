e91a1aed38123a719e72e838f6e54a9f2ae873a3
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 13:06:41 +0200

    unordered collection, unique elements

---
+++ lists/sets.py
@@ -0,0 +1,16 @@
+# unordered collections of unique elements =< 1 of same object
+
+myset = set() # set
+print(myset)
+
+myset.add(1)
+print(myset)
+
+myset.add(2)
+print(myset)
+
+myset.add(2) # no error
+print(myset)
+
+mylist = [1,1,1,1,1,2,2,3,3,3]
+print(set(mylist))
